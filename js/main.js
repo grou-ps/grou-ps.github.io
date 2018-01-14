e0f87987c71022e8a9a5d2f9d8487749125a2fc2
Groups <esokullu@gmail.com>
2018-01-14 11:58:30 -0800

    css updated

--- js/main.js
+++ js/main.js
@@ -28,3 +28,37 @@ $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
 	});
 });
 } )( jQuery );
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+document.getElementById("myVideo").playbackRate = 2.5;
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
